4b969f0343012b1f02fecfce05c0d98c6f351edb
Chris <chrislyko@gmail.com>
2021-09-07 07:30:39 -0400

    populated mock-tasks with 6 random tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,38 @@
+export const TASKS=[
+    {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'September 1st',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Dentist Appointment',
+        day: 'September 8th',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Get Groceries',
+        day: 'September 3rd',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "12 O'clock Meeting",
+        day: 'September 5th',
+        reminder: true
+    },
+    {
+        id: 5,
+        text: 'Pick Up Medicine',
+        day: 'September 10th',
+        reminder: false
+    },
+    {
+        id: 6,
+        text: 'Get Dog Groomed',
+        day: 'September 21st',
+        reminder: true
+    },
+]
